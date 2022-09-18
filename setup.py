3aefa8d8516efeda55b1cba3f69d1d45c0451765
Aurora <aurorarivas1606@gmail.com>
2022-09-18 01:16:52 -0500

    module test v1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name="Data relations", 
+setup(name="Data_relations", 
     version="0", 
     description="Obtención de datos", 
     author="Aurora Martínez",
     author_email='aurorarivas1606@gmail.com',
-    packages=['COVID_data'],
+    packages=['Data_relations'],
     install_requires=['requests', 'pandas', 'numpy'],
 )
