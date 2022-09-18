93148cb03ae6b4f4a0a2d27bddd0ad7dce520a02
Aurora <aurorarivas1606@gmail.com>
2022-09-18 00:42:51 -0500

    module test v5

---
+++ COVID_data/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="Data relations", 
+    version="0", 
+    description="Obtención de datos", 
+    author="Aurora Martínez",
+    author_email='aurorarivas1606@gmail.com',
+    packages=['Data_relations'],
+    install_requires=['requests', 'pandas', 'numpy'],
+)
